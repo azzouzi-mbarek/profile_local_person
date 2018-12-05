50fd888c1b7e2013aa891f4a360b86109053c2ac
mohamed <jemmal>
2018-12-05 01:15:26 +0000

    serach and navigate

--- frontend/src/app/landing/directory/directory.component.ts
+++ frontend/src/app/landing/directory/directory.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
 import { DirectoryService } from 'src/app/services/directory.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-directory',
@@ -13,25 +14,43 @@ export class DirectoryComponent implements OnInit {
   results = [];
   levelsResults = [];
   personsResults = [];
+  resultsCounts = false;
+  counts: number = null;
+  searchTerm = null;
 
-  counts = 0;
-  searchTerm: string;
-
+  // get searchTerm(): String {
+  //   return this._searchTerm;
+  // }
+  // set searchTerm(value: String) {
+  //   this._searchTerm = value;
+  //   this.search();
+  // }
 
 
   constructor(
     private http: HttpClient,
-    private _directoryService: DirectoryService) { }
+    private _directoryService: DirectoryService,
+    private _router: Router) { }
 
   ngOnInit() {
   }
 
   search() {
+
     this._directoryService.getData(this.searchTerm).subscribe(
       (data: any) => {
         this.results = data.data;
-        console.log(this.levelsResults = this.results['levels']);
-        console.log(this.personsResults = this.results['persons']);
+
+        this.levelsResults = this.results['levels'];
+        console.log(this.levelsResults);
+        this.personsResults = this.results['persons'];
+
+        if (this.levelsResults.length > 0 || this.personsResults.length > 0) {
+          this.resultsCounts = true;
+          this.counts = this.levelsResults.length + this.personsResults.length;
+        } else if (this.levelsResults.length < 0 && this.personsResults.length < 0) {
+          this.resultsCounts = false;
+        }
 
 
       },
@@ -40,10 +59,15 @@ export class DirectoryComponent implements OnInit {
   }
 
 
-  gotPersonProfil(id) {
+  goToPersonProfil(id) {
+    this._router.navigate(['landing/profile-person', id]);
 
   }
 
+
+  goToLevelProfil(id) {
+    this._router.navigate(['landing/profile-local-government', id]);
+  }
 
 
 }
